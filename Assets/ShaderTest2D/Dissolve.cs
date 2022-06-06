3a7a5941fb49756332e2dea2bf66e63fac16cc70
pedrohfcarneiro <phtriplox8090@gmail.com>
2022-06-06 18:33:02 -0300

    add project to github

---
+++ Assets/ShaderTest2D/Dissolve.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dissolve : MonoBehaviour
+{
+    Material material;
+
+    bool isDissolving = false;
+    float fade = 1f;
+
+
+    void Start()
+    {
+        material = GetComponent<SpriteRenderer>().material;    
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            isDissolving = true;
+        }
+
+        if(isDissolving)
+        {
+            fade -= Time.deltaTime;
+
+            if(fade <= 0f)
+            {
+                fade = 0;
+                isDissolving = false;
+            }
+        }
+
+        // Set the property
+        material.SetFloat("_Fade", fade);
+    }
+}
